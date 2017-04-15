a9902067c6a4a0093d42b026ac2110244f5462a6
begogna <begmeil@gmail.com>
2017-04-15 20:38:07 +0200

    categories added

--- js/script.js
+++ js/script.js
@@ -39,11 +39,19 @@ var switchMenuToActive = function () {
 
 (function (global) {
 
+    //Example conversion JSON -> Javascript object
+    // var myJSON = '{ "name":"John", "age":31, "city":"New York" }';
+    // var myObj = JSON.parse(myJSON);
+    // document.getElementById("demo").innerHTML = myObj.name;
+
     //Context variable to allow using the properties in other pages or scripts
     var cbb = {};
     var homeHtml = "snippets/home-snippet.html";
     var aboutHtml = "snippets/about-snippet.html";
-
+    var categoryHtml = "snippets/category-snippet.html";
+    //var allCategoriesUrl = "https://creabybegoapp.herokuapp.com/categories.json";
+    var allCategoriesUrl = "categories.json";
+    var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 
     // Convenience function for inserting innerHTML for 'select'
     var insertHtml = function (selector, html) {
@@ -57,6 +65,15 @@ var switchMenuToActive = function () {
       html += "<i class='fa fa-cog fa-spin fa-3x fa-fw'></i></div>";
       insertHtml(selector, html);
     };
+
+    // Return substitute of '{{propName}}'
+    // with propValue in given 'string'
+    var insertProperty = function (string, propName, propValue) {
+      var propToReplace = "{{" + propName + "}}";
+      string = string
+        .replace(new RegExp(propToReplace, "g"), propValue);
+      return string;
+    }
 
     // On page load (before images or CSS)
     document.addEventListener("DOMContentLoaded", function (event) {
@@ -85,6 +102,80 @@ var switchMenuToActive = function () {
         },
         false);
     };
+
+
+// Load the menu categories view
+cbb.loadCategories = function () {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(
+    allCategoriesUrl,
+    buildAndShowCategoriesHTML);
+};
+
+
+// Load the menu items view
+// 'categoryShort' is a short_name for a category
+cbb.loadMenuItems = function (categoryShort) {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(
+    menuItemsUrl + categoryShort,
+    buildAndShowMenuItemsHTML);
+};
+
+// Builds HTML for the categories page based on the data
+// from the server
+function buildAndShowCategoriesHTML (categories) {
+  // Load title snippet of categories page
+  $ajaxUtils.sendGetRequest(
+    categoriesTitleHtml,
+    function (categoriesTitleHtml) {
+      // Retrieve single category snippet
+      $ajaxUtils.sendGetRequest(
+        categoryHtml,
+        function (categoryHtml) {
+          // Switch CSS class active to menu button
+          switchMenuToActive();
+
+          var categoriesViewHtml =
+            buildCategoriesViewHtml(categories,
+                                    categoriesTitleHtml,
+                                    categoryHtml);
+          insertHtml("#main-content", categoriesViewHtml);
+        },
+        false);
+    },
+    false);
+}
+
+
+// Using categories data and snippets html
+// build categories view HTML to be inserted into page
+function buildCategoriesViewHtml(categories,
+                                 categoriesTitleHtml,
+                                 categoryHtml) {
+
+  var finalHtml = categoriesTitleHtml;
+  finalHtml += "<section class='row'>";
+
+  // Loop over categories
+  for (var i = 0; i < categories.length; i++) {
+    // Insert category values
+    var html = categoryHtml;
+    var name = "" + categories[i].name;
+    var short_name = categories[i].short_name;
+    html =
+      insertProperty(html, "name", name);
+    html =
+      insertProperty(html,
+                     "short_name",
+                     short_name);
+    finalHtml += html;
+  }
+
+  finalHtml += "</section>";
+  return finalHtml;
+}
+
 
     global.$cbb = cbb;
 
